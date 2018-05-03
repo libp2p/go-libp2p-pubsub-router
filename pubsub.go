dd42345efa612e280d8f4d5f56018e17227bccde
Steven Allen <steven@stebalien.com>
2018-05-02 18:13:42 -0700

    convert to a ValueStore and finalize as a separate package

--- pubsub.go
+++ pubsub.go
@@ -1,284 +1,222 @@
 package namesys
 
 import (
+	"bytes"
 	"context"
-	"errors"
-	"fmt"
-	"strings"
 	"sync"
 	"time"
 
-	opts "github.com/ipfs/go-ipfs/namesys/opts"
-	pb "github.com/ipfs/go-ipfs/namesys/pb"
-	path "github.com/ipfs/go-ipfs/path"
-
-	proto "github.com/gogo/protobuf/proto"
 	cid "github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
 	dssync "github.com/ipfs/go-datastore/sync"
 	dshelp "github.com/ipfs/go-ipfs-ds-help"
 	u "github.com/ipfs/go-ipfs-util"
+	logging "github.com/ipfs/go-log"
 	floodsub "github.com/libp2p/go-floodsub"
-	ci "github.com/libp2p/go-libp2p-crypto"
 	p2phost "github.com/libp2p/go-libp2p-host"
-	peer "github.com/libp2p/go-libp2p-peer"
 	pstore "github.com/libp2p/go-libp2p-peerstore"
 	record "github.com/libp2p/go-libp2p-record"
-	dhtpb "github.com/libp2p/go-libp2p-record/pb"
 	routing "github.com/libp2p/go-libp2p-routing"
-	mh "github.com/multiformats/go-multihash"
+	ropts "github.com/libp2p/go-libp2p-routing/options"
 )
 
-// PubsubPublisher is a publisher that distributes IPNS records through pubsub
-type PubsubPublisher struct {
+var log = logging.Logger("pubsub-valuestore")
+
+type PubsubValueStore struct {
 	ctx  context.Context
 	ds   ds.Datastore
 	host p2phost.Host
 	cr   routing.ContentRouting
 	ps   *floodsub.PubSub
 
-	mx   sync.Mutex
-	subs map[string]struct{}
-}
-
-// PubsubResolver is a resolver that receives IPNS records through pubsub
-type PubsubResolver struct {
-	ctx  context.Context
-	ds   ds.Datastore
-	host p2phost.Host
-	cr   routing.ContentRouting
-	pkf  routing.PubKeyFetcher
-	ps   *floodsub.PubSub
-
+	// Map of keys to subscriptions.
+	//
+	// If a key is present but the subscription is nil, we've bootstrapped
+	// but haven't subscribed.
 	mx   sync.Mutex
 	subs map[string]*floodsub.Subscription
+
+	Validator record.Validator
+	Selector  record.Selector
 }
 
 // NewPubsubPublisher constructs a new Publisher that publishes IPNS records through pubsub.
 // The constructor interface is complicated by the need to bootstrap the pubsub topic.
 // This could be greatly simplified if the pubsub implementation handled bootstrap itself
-func NewPubsubPublisher(ctx context.Context, host p2phost.Host, ds ds.Datastore, cr routing.ContentRouting, ps *floodsub.PubSub) *PubsubPublisher {
-	return &PubsubPublisher{
-		ctx:  ctx,
-		ds:   ds,
-		host: host, // needed for pubsub bootstrap
-		cr:   cr,   // needed for pubsub bootstrap
-		ps:   ps,
-		subs: make(map[string]struct{}),
-	}
-}
-
-// NewPubsubResolver constructs a new Resolver that resolves IPNS records through pubsub.
-// same as above for pubsub bootstrap dependencies
-func NewPubsubResolver(ctx context.Context, host p2phost.Host, cr routing.ContentRouting, pkf routing.PubKeyFetcher, ps *floodsub.PubSub) *PubsubResolver {
-	return &PubsubResolver{
+func NewPubsubValueStore(ctx context.Context, host p2phost.Host, cr routing.ContentRouting, ps *floodsub.PubSub) *PubsubValueStore {
+	return &PubsubValueStore{
 		ctx:  ctx,
 		ds:   dssync.MutexWrap(ds.NewMapDatastore()),
 		host: host, // needed for pubsub bootstrap
 		cr:   cr,   // needed for pubsub bootstrap
-		pkf:  pkf,
 		ps:   ps,
 		subs: make(map[string]*floodsub.Subscription),
 	}
 }
 
 // Publish publishes an IPNS record through pubsub with default TTL
-func (p *PubsubPublisher) Publish(ctx context.Context, k ci.PrivKey, value path.Path) error {
-	return p.PublishWithEOL(ctx, k, value, time.Now().Add(DefaultRecordTTL))
-}
-
-// PublishWithEOL publishes an IPNS record through pubsub
-func (p *PubsubPublisher) PublishWithEOL(ctx context.Context, k ci.PrivKey, value path.Path, eol time.Time) error {
-	id, err := peer.IDFromPrivateKey(k)
-	if err != nil {
-		return err
-	}
-
-	_, ipnskey := IpnsKeysForID(id)
-
-	seqno, err := p.getPreviousSeqNo(ctx, ipnskey)
-	if err != nil {
-		return err
-	}
-
-	seqno++
-
-	return p.publishRecord(ctx, k, value, seqno, eol, ipnskey, id)
-}
-
-func (p *PubsubPublisher) getPreviousSeqNo(ctx context.Context, ipnskey string) (uint64, error) {
-	// the datastore is shared with the routing publisher to properly increment and persist
-	// ipns record sequence numbers.
-	prevrec, err := p.ds.Get(dshelp.NewKeyFromBinary([]byte(ipnskey)))
-	if err != nil {
-		if err == ds.ErrNotFound {
-			// None found, lets start at zero!
-			return 0, nil
-		}
-		return 0, err
-	}
-
-	prbytes, ok := prevrec.([]byte)
-	if !ok {
-		return 0, fmt.Errorf("unexpected type returned from datastore: %#v", prevrec)
-	}
-
-	var dsrec dhtpb.Record
-	err = proto.Unmarshal(prbytes, &dsrec)
-	if err != nil {
-		return 0, err
-	}
-
-	var entry pb.IpnsEntry
-	err = proto.Unmarshal(dsrec.GetValue(), &entry)
-	if err != nil {
-		return 0, err
-	}
-
-	return entry.GetSequence(), nil
-}
-
-func (p *PubsubPublisher) publishRecord(ctx context.Context, k ci.PrivKey, value path.Path, seqno uint64, eol time.Time, ipnskey string, ID peer.ID) error {
-	entry, err := CreateRoutingEntryData(k, value, seqno, eol)
-	if err != nil {
-		return err
-	}
-
-	data, err := proto.Marshal(entry)
-	if err != nil {
-		return err
-	}
-
-	// the datastore is shared with the routing publisher to properly increment and persist
-	// ipns record sequence numbers; so we need to Record our new entry in the datastore
-	dsrec, err := record.MakePutRecord(k, ipnskey, data, true)
-	if err != nil {
-		return err
-	}
-
-	dsdata, err := proto.Marshal(dsrec)
-	if err != nil {
-		return err
-	}
-
-	err = p.ds.Put(dshelp.NewKeyFromBinary([]byte(ipnskey)), dsdata)
-	if err != nil {
-		return err
-	}
-
-	// now we publish, but we also need to bootstrap pubsub for our messages to propagate
-	topic := "/ipns/" + ID.Pretty()
-
+func (p *PubsubValueStore) PutValue(ctx context.Context, key string, value []byte, opts ...ropts.Option) error {
 	p.mx.Lock()
-	_, ok := p.subs[topic]
+	_, bootstraped := p.subs[key]
 
-	if !ok {
-		p.subs[topic] = struct{}{}
+	if !bootstraped {
+		p.subs[key] = nil
 		p.mx.Unlock()
 
-		bootstrapPubsub(p.ctx, p.cr, p.host, topic)
+		bootstrapPubsub(p.ctx, p.cr, p.host, key)
 	} else {
 		p.mx.Unlock()
 	}
 
-	log.Debugf("PubsubPublish: publish IPNS record for %s (%d)", topic, seqno)
-	return p.ps.Publish(topic, data)
+	log.Debugf("PubsubPublish: publish value for key", key)
+	return p.ps.Publish(key, value)
 }
 
-// Resolve resolves a name through pubsub and default depth limit
-func (r *PubsubResolver) Resolve(ctx context.Context, name string, options ...opts.ResolveOpt) (path.Path, error) {
-	return resolve(ctx, r, name, opts.ProcessOpts(options), "/ipns/")
-}
-
-func (r *PubsubResolver) resolveOnce(ctx context.Context, name string, options *opts.ResolveOpts) (path.Path, error) {
-	log.Debugf("PubsubResolve: resolve '%s'", name)
-
-	// retrieve the public key once (for verifying messages)
-	xname := strings.TrimPrefix(name, "/ipns/")
-	hash, err := mh.FromB58String(xname)
+func (p *PubsubValueStore) isBetter(key string, val []byte) bool {
+	ns, name, err := record.SplitKey(key)
 	if err != nil {
-		log.Warningf("PubsubResolve: bad input hash: [%s]", xname)
-		return "", err
+		return false
+	}
+	selector, ok1 := p.Selector[ns]
+	validator, ok2 := p.Validator[ns]
+	if !(ok1 && ok2) {
+		return false
 	}
 
-	id := peer.ID(hash)
-	if r.host.Peerstore().PrivKey(id) != nil {
-		return "", errors.New("cannot resolve own name through pubsub")
+	if validator(&record.ValidationRecord{
+		Namespace: ns,
+		Key:       name,
+		Value:     val,
+	}) != nil {
+		return false
 	}
 
-	pubk := id.ExtractPublicKey()
-	if pubk == nil {
-		pubk, err = r.pkf.GetPublicKey(ctx, id)
-		if err != nil {
-			log.Warningf("PubsubResolve: error fetching public key: %s [%s]", err.Error(), xname)
-			return "", err
-		}
+	old, err := p.getLocal(key)
+	if err != nil {
+		// If the old one is invalid, the new one is *always* better.
+		return true
 	}
 
-	// the topic is /ipns/Qmhash
-	if !strings.HasPrefix(name, "/ipns/") {
-		name = "/ipns/" + name
+	// Same record. Possible DoS vector, should consider failing?
+	if bytes.Equal(old, val) {
+		return true
 	}
 
-	r.mx.Lock()
+	i, err := selector(key, [][]byte{val, old})
+	return err == nil && i == 0
+}
+
+func (p *PubsubValueStore) canValidate(key string) error {
+	ns, _, err := record.SplitKey(key)
+	if err != nil {
+		return err
+	}
+
+	_, ok1 := p.Selector[ns]
+	_, ok2 := p.Validator[ns]
+	if !(ok1 && ok2) {
+		return record.ErrInvalidRecordType
+	}
+	return nil
+}
+
+func (p *PubsubValueStore) Subscribe(key string) error {
+	if err := p.canValidate(key); err != nil {
+		return err
+	}
+
+	p.mx.Lock()
 	// see if we already have a pubsub subscription; if not, subscribe
-	sub, ok := r.subs[name]
+	sub := p.subs[key]
+	p.mx.Unlock()
+
+	if sub != nil {
+		return nil
+	}
+
+	// Ignore the error. We have to check again anyways to make sure the
+	// record hasn't expired.
+	//
+	// Also, make sure to do this *before* subscribing.
+	p.ps.RegisterTopicValidator(key, func(ctx context.Context, msg *floodsub.Message) bool {
+		return p.isBetter(key, msg.GetData())
+	})
+
+	sub, err := p.ps.Subscribe(key)
+	if err != nil {
+		p.mx.Unlock()
+		return err
+	}
+
+	p.mx.Lock()
+	existingSub, bootstraped := p.subs[key]
+	if existingSub != nil {
+		p.mx.Unlock()
+		sub.Cancel()
+		return nil
+	}
+
+	p.subs[key] = sub
+	ctx, cancel := context.WithCancel(p.ctx)
+	go p.handleSubscription(sub, key, cancel)
+	p.mx.Unlock()
+
+	log.Debugf("PubsubResolve: subscribed to %s", key)
+
+	if !bootstraped {
+		// TODO: Deal with publish then resolve case? Cancel behaviour changes.
+		go bootstrapPubsub(ctx, p.cr, p.host, key)
+	}
+	return nil
+}
+
+func (p *PubsubValueStore) getLocal(key string) ([]byte, error) {
+	ns, name, err := record.SplitKey(key)
+	if err != nil {
+		return nil, err
+	}
+
+	validator, ok := p.Validator[ns]
 	if !ok {
-		sub, err = r.ps.Subscribe(name)
-		if err != nil {
-			r.mx.Unlock()
-			return "", err
-		}
-
-		log.Debugf("PubsubResolve: subscribed to %s", name)
-
-		r.subs[name] = sub
-
-		ctx, cancel := context.WithCancel(r.ctx)
-		go r.handleSubscription(sub, name, pubk, cancel)
-		go bootstrapPubsub(ctx, r.cr, r.host, name)
+		return nil, record.ErrInvalidRecordType
 	}
-	r.mx.Unlock()
 
-	// resolve to what we may already have in the datastore
-	dsval, err := r.ds.Get(dshelp.NewKeyFromBinary([]byte(name)))
+	dsval, err := p.ds.Get(dshelp.NewKeyFromBinary([]byte(key)))
 	if err != nil {
+		// Don't invalidate due to ds errors.
 		if err == ds.ErrNotFound {
-			return "", ErrResolveFailed
+			err = routing.ErrNotFound
 		}
-		return "", err
+		return nil, err
+	}
+	val := dsval.([]byte)
+
+	// If the old one is invalid, the new one is *always* better.
+	if err := validator(&record.ValidationRecord{
+		Namespace: ns,
+		Key:       name,
+		Value:     val,
+	}); err != nil {
+		return nil, err
+	}
+	return val, nil
+}
+
+func (p *PubsubValueStore) GetValue(ctx context.Context, key string, opts ...ropts.Option) ([]byte, error) {
+	if err := p.Subscribe(key); err != nil {
+		return nil, err
 	}
 
-	data := dsval.([]byte)
-	entry := new(pb.IpnsEntry)
-
-	err = proto.Unmarshal(data, entry)
-	if err != nil {
-		return "", err
-	}
-
-	// check EOL; if the entry has expired, delete from datastore and return ds.ErrNotFound
-	eol, ok := checkEOL(entry)
-	if ok && eol.Before(time.Now()) {
-		err = r.ds.Delete(dshelp.NewKeyFromBinary([]byte(name)))
-		if err != nil {
-			log.Warningf("PubsubResolve: error deleting stale value for %s: %s", name, err.Error())
-		}
-
-		return "", ErrResolveFailed
-	}
-
-	value, err := path.ParsePath(string(entry.GetValue()))
-	return value, err
+	return p.getLocal(key)
 }
 
 // GetSubscriptions retrieves a list of active topic subscriptions
-func (r *PubsubResolver) GetSubscriptions() []string {
-	r.mx.Lock()
-	defer r.mx.Unlock()
+func (p *PubsubValueStore) GetSubscriptions() []string {
+	p.mx.Lock()
+	defer p.mx.Unlock()
 
 	var res []string
-	for sub := range r.subs {
+	for sub := range p.subs {
 		res = append(res, sub)
 	}
 
@@ -287,85 +225,38 @@ func (r *PubsubResolver) GetSubscriptions() []string {
 
 // Cancel cancels a topic subscription; returns true if an active
 // subscription was canceled
-func (r *PubsubResolver) Cancel(name string) bool {
-	r.mx.Lock()
-	defer r.mx.Unlock()
+func (p *PubsubValueStore) Cancel(name string) bool {
+	p.mx.Lock()
+	defer p.mx.Unlock()
 
-	sub, ok := r.subs[name]
+	sub, ok := p.subs[name]
 	if ok {
 		sub.Cancel()
-		delete(r.subs, name)
+		delete(p.subs, name)
 	}
 
 	return ok
 }
 
-func (r *PubsubResolver) handleSubscription(sub *floodsub.Subscription, name string, pubk ci.PubKey, cancel func()) {
+func (p *PubsubValueStore) handleSubscription(sub *floodsub.Subscription, key string, cancel func()) {
 	defer sub.Cancel()
 	defer cancel()
 
 	for {
-		msg, err := sub.Next(r.ctx)
+		msg, err := sub.Next(p.ctx)
 		if err != nil {
 			if err != context.Canceled {
-				log.Warningf("PubsubResolve: subscription error in %s: %s", name, err.Error())
+				log.Warningf("PubsubResolve: subscription error in %s: %s", key, err.Error())
 			}
 			return
 		}
-
-		err = r.receive(msg, name, pubk)
-		if err != nil {
-			log.Warningf("PubsubResolve: error processing update for %s: %s", name, err.Error())
+		if p.isBetter(key, msg.GetData()) {
+			err := p.ds.Put(dshelp.NewKeyFromBinary([]byte(key)), msg.GetData())
+			if err != nil {
+				log.Warningf("PubsubResolve: error writing update for %s: %s", key, err)
+			}
 		}
 	}
-}
-
-func (r *PubsubResolver) receive(msg *floodsub.Message, name string, pubk ci.PubKey) error {
-	data := msg.GetData()
-	if data == nil {
-		return errors.New("empty message")
-	}
-
-	entry := new(pb.IpnsEntry)
-	err := proto.Unmarshal(data, entry)
-	if err != nil {
-		return err
-	}
-
-	ok, err := pubk.Verify(ipnsEntryDataForSig(entry), entry.GetSignature())
-	if err != nil || !ok {
-		return errors.New("signature verification failed")
-	}
-
-	_, err = path.ParsePath(string(entry.GetValue()))
-	if err != nil {
-		return err
-	}
-
-	eol, ok := checkEOL(entry)
-	if ok && eol.Before(time.Now()) {
-		return errors.New("stale update; EOL exceeded")
-	}
-
-	// check the sequence number against what we may already have in our datastore
-	oval, err := r.ds.Get(dshelp.NewKeyFromBinary([]byte(name)))
-	if err == nil {
-		odata := oval.([]byte)
-		oentry := new(pb.IpnsEntry)
-
-		err = proto.Unmarshal(odata, oentry)
-		if err != nil {
-			return err
-		}
-
-		if entry.GetSequence() <= oentry.GetSequence() {
-			return errors.New("stale update; sequence number too small")
-		}
-	}
-
-	log.Debugf("PubsubResolve: receive IPNS record for %s", name)
-
-	return r.ds.Put(dshelp.NewKeyFromBinary([]byte(name)), data)
 }
 
 // rendezvous with peers in the name topic through provider records
--- pubsub_test.go
+++ pubsub_test.go
@@ -1,24 +1,18 @@
 package namesys
 
 import (
+	"bytes"
 	"context"
-	"sync"
 	"testing"
 	"time"
 
-	path "github.com/ipfs/go-ipfs/path"
-
-	ds "github.com/ipfs/go-datastore"
-	mockrouting "github.com/ipfs/go-ipfs-routing/mock"
 	floodsub "github.com/libp2p/go-floodsub"
 	bhost "github.com/libp2p/go-libp2p-blankhost"
-	ci "github.com/libp2p/go-libp2p-crypto"
 	p2phost "github.com/libp2p/go-libp2p-host"
 	netutil "github.com/libp2p/go-libp2p-netutil"
-	peer "github.com/libp2p/go-libp2p-peer"
-	pstore "github.com/libp2p/go-libp2p-peerstore"
+	record "github.com/libp2p/go-libp2p-record"
 	routing "github.com/libp2p/go-libp2p-routing"
-	testutil "github.com/libp2p/go-testutil"
+	rhelper "github.com/libp2p/go-libp2p-routing-helpers"
 )
 
 func newNetHost(ctx context.Context, t *testing.T) p2phost.Host {
@@ -37,98 +31,68 @@ func newNetHosts(ctx context.Context, t *testing.T, n int) []p2phost.Host {
 	return out
 }
 
-// PubKeyFetcher implementation with a global key store
-type mockKeyStore struct {
-	keys map[peer.ID]ci.PubKey
-	mx   sync.Mutex
-}
-
-func (m *mockKeyStore) addPubKey(id peer.ID, pkey ci.PubKey) {
-	m.mx.Lock()
-	defer m.mx.Unlock()
-	m.keys[id] = pkey
-}
-
-func (m *mockKeyStore) getPubKey(id peer.ID) (ci.PubKey, error) {
-	m.mx.Lock()
-	defer m.mx.Unlock()
-	pkey, ok := m.keys[id]
-	if ok {
-		return pkey, nil
-	}
-
-	return nil, routing.ErrNotFound
-}
-
-func (m *mockKeyStore) GetPublicKey(ctx context.Context, id peer.ID) (ci.PubKey, error) {
-	return m.getPubKey(id)
-}
-
-func newMockKeyStore() *mockKeyStore {
-	return &mockKeyStore{
-		keys: make(map[peer.ID]ci.PubKey),
-	}
-}
-
-// ConentRouting mock
-func newMockRouting(ms mockrouting.Server, ks *mockKeyStore, host p2phost.Host) routing.ContentRouting {
-	id := host.ID()
-
-	privk := host.Peerstore().PrivKey(id)
-	pubk := host.Peerstore().PubKey(id)
-	pi := host.Peerstore().PeerInfo(id)
-
-	ks.addPubKey(id, pubk)
-	return ms.Client(testutil.NewIdentity(id, pi.Addrs[0], privk, pubk))
-}
-
-func newMockRoutingForHosts(ms mockrouting.Server, ks *mockKeyStore, hosts []p2phost.Host) []routing.ContentRouting {
-	rs := make([]routing.ContentRouting, len(hosts))
-	for i := 0; i < len(hosts); i++ {
-		rs[i] = newMockRouting(ms, ks, hosts[i])
-	}
-	return rs
-}
-
 // tests
 func TestPubsubPublishSubscribe(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	ms := mockrouting.NewServer()
-	ks := newMockKeyStore()
+	key := "/namespace/key"
+	key2 := "/namespace/key2"
 
-	pubhost := newNetHost(ctx, t)
-	pubmr := newMockRouting(ms, ks, pubhost)
-	fs, err := floodsub.NewFloodSub(ctx, pubhost)
-	if err != nil {
-		t.Fatal(err)
-	}
-	pub := NewPubsubPublisher(ctx, pubhost, ds.NewMapDatastore(), pubmr, fs)
-	privk := pubhost.Peerstore().PrivKey(pubhost.ID())
-	pubpinfo := pstore.PeerInfo{ID: pubhost.ID(), Addrs: pubhost.Addrs()}
+	hosts := newNetHosts(ctx, t, 5)
+	vss := make([]*PubsubValueStore, len(hosts))
+	for i := 0; i < len(vss); i++ {
 
-	name := "/ipns/" + pubhost.ID().Pretty()
-
-	reshosts := newNetHosts(ctx, t, 5)
-	resmrs := newMockRoutingForHosts(ms, ks, reshosts)
-	res := make([]*PubsubResolver, len(reshosts))
-	for i := 0; i < len(res); i++ {
-
-		fs, err := floodsub.NewFloodSub(ctx, reshosts[i])
+		fs, err := floodsub.NewFloodSub(ctx, hosts[i])
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		res[i] = NewPubsubResolver(ctx, reshosts[i], resmrs[i], ks, fs)
-		if err := reshosts[i].Connect(ctx, pubpinfo); err != nil {
+		vss[i] = NewPubsubValueStore(ctx, hosts[i], rhelper.Null{}, fs)
+		vss[i].Validator = record.Validator{
+			"namespace": func(r *record.ValidationRecord) error {
+				if r.Namespace != "namespace" {
+					return record.ErrInvalidRecordType
+				}
+				if !bytes.Contains(r.Value, []byte(r.Key)) {
+					return record.ErrInvalidRecordType
+				}
+				if bytes.Contains(r.Value, []byte("invalid")) {
+					return record.ErrInvalidRecordType
+				}
+				return nil
+			},
+		}
+		vss[i].Selector = record.Selector{
+			"namespace": func(key string, vals [][]byte) (int, error) {
+				if len(vals) == 0 {
+					panic("selector with no values")
+				}
+				var best []byte
+				idx := 0
+				for i, val := range vals {
+					if bytes.Compare(best, val) < 0 {
+						best = val
+						idx = i
+					}
+				}
+				return idx, nil
+			},
+		}
+	}
+	pub := vss[0]
+	vss = vss[1:]
+
+	pubinfo := hosts[0].Peerstore().PeerInfo(hosts[0].ID())
+	for _, h := range hosts[1:] {
+		if err := h.Connect(ctx, pubinfo); err != nil {
 			t.Fatal(err)
 		}
 	}
 
 	time.Sleep(time.Millisecond * 100)
-	for i := 0; i < len(res); i++ {
-		checkResolveNotFound(ctx, t, i, res[i], name)
+	for i, vs := range vss {
+		checkNotFound(ctx, t, i, vs, key)
 		// delay to avoid connection storms
 		time.Sleep(time.Millisecond * 100)
 	}
@@ -136,62 +100,112 @@ func TestPubsubPublishSubscribe(t *testing.T) {
 	// let the bootstrap finish
 	time.Sleep(time.Second * 1)
 
-	val := path.Path("/ipfs/QmP1DfoUjiWH2ZBo1PBH6FupdBucbDepx3HpWmEY6JMUpY")
-	err = pub.Publish(ctx, privk, val)
+	val := []byte("valid for key 1")
+	err := pub.PutValue(ctx, key, val)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// let the flood propagate
 	time.Sleep(time.Second * 1)
-	for i := 0; i < len(res); i++ {
-		checkResolve(ctx, t, i, res[i], name, val)
+	for i, vs := range vss {
+		checkValue(ctx, t, i, vs, key, val)
 	}
 
-	val = path.Path("/ipfs/QmP1wMAqk6aZYRZirbaAwmrNeqFRgQrwBt3orUtvSa1UYD")
-	err = pub.Publish(ctx, privk, val)
+	val = []byte("valid for key 2")
+	err = pub.PutValue(ctx, key, val)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// let the flood propagate
 	time.Sleep(time.Second * 1)
-	for i := 0; i < len(res); i++ {
-		checkResolve(ctx, t, i, res[i], name, val)
+	for i, vs := range vss {
+		checkValue(ctx, t, i, vs, key, val)
+	}
+
+	// Check selector.
+	nval := []byte("valid for key 1")
+	err = pub.PutValue(ctx, key, val)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// let the flood propagate
+	time.Sleep(time.Second * 1)
+	for i, vs := range vss {
+		checkValue(ctx, t, i, vs, key, val)
+	}
+
+	// Check validator.
+	nval = []byte("valid for key 9999 invalid")
+	err = pub.PutValue(ctx, key, val)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// let the flood propagate
+	time.Sleep(time.Second * 1)
+	for i, vs := range vss {
+		checkValue(ctx, t, i, vs, key, val)
+	}
+
+	// Different key?
+
+	// subscribe to the second key
+	for i, vs := range vss {
+		checkNotFound(ctx, t, i, vs, key2)
+	}
+	time.Sleep(time.Second * 1)
+
+	// Put to the second key
+	nval = []byte("valid for key2")
+	err = pub.PutValue(ctx, key2, nval)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// let the flood propagate
+	time.Sleep(time.Second * 1)
+	for i, vs := range vss {
+		checkValue(ctx, t, i, vs, key2, nval)
+		checkValue(ctx, t, i, vs, key, val)
 	}
 
 	// cancel subscriptions
-	for i := 0; i < len(res); i++ {
-		res[i].Cancel(name)
+	for _, vs := range vss {
+		vs.Cancel(key)
 	}
 	time.Sleep(time.Millisecond * 100)
 
-	nval := path.Path("/ipfs/QmPgDWmTmuzvP7QE5zwo1TmjbJme9pmZHNujB2453jkCTr")
-	err = pub.Publish(ctx, privk, nval)
+	nval = []byte("valid for key 3")
+	err = pub.PutValue(ctx, key, nval)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// check we still have the old value in the resolver
+	// check we still have the old value in the vssolver
 	time.Sleep(time.Second * 1)
-	for i := 0; i < len(res); i++ {
-		checkResolve(ctx, t, i, res[i], name, val)
+	for i, vs := range vss {
+		checkValue(ctx, t, i, vs, key, val)
 	}
 }
 
-func checkResolveNotFound(ctx context.Context, t *testing.T, i int, resolver Resolver, name string) {
-	_, err := resolver.Resolve(ctx, name)
-	if err != ErrResolveFailed {
-		t.Fatalf("[resolver %d] unexpected error: %s", i, err.Error())
+func checkNotFound(ctx context.Context, t *testing.T, i int, vs routing.ValueStore, key string) {
+	t.Helper()
+	_, err := vs.GetValue(ctx, key)
+	if err != routing.ErrNotFound {
+		t.Fatalf("[vssolver %d] unexpected error: %s", i, err.Error())
 	}
 }
 
-func checkResolve(ctx context.Context, t *testing.T, i int, resolver Resolver, name string, val path.Path) {
-	xval, err := resolver.Resolve(ctx, name)
+func checkValue(ctx context.Context, t *testing.T, i int, vs routing.ValueStore, key string, val []byte) {
+	t.Helper()
+	xval, err := vs.GetValue(ctx, key)
 	if err != nil {
-		t.Fatalf("[resolver %d] resolve failed: %s", i, err.Error())
+		t.Fatalf("[ValueStore %d] vssolve failed: %s", i, err.Error())
 	}
-	if xval != val {
-		t.Fatalf("[resolver %d] unexpected value: %s %s", i, val, xval)
+	if !bytes.Equal(xval, val) {
+		t.Fatalf("[ValueStore %d] unexpected value: expected '%s', got '%s'", i, val, xval)
 	}
 }
