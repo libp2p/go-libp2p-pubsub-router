5d729335a7132555b7d4373a7114a33df054e31a
Steven Allen <steven@stebalien.com>
2018-06-05 15:57:58 -0700

    fixup for transport refactor

--- pubsub_test.go
+++ pubsub_test.go
@@ -9,14 +9,14 @@ import (
 	floodsub "github.com/libp2p/go-floodsub"
 	bhost "github.com/libp2p/go-libp2p-blankhost"
 	p2phost "github.com/libp2p/go-libp2p-host"
-	netutil "github.com/libp2p/go-libp2p-netutil"
 	record "github.com/libp2p/go-libp2p-record"
 	routing "github.com/libp2p/go-libp2p-routing"
 	rhelper "github.com/libp2p/go-libp2p-routing-helpers"
+	swarmt "github.com/libp2p/go-libp2p-swarm/testing"
 )
 
 func newNetHost(ctx context.Context, t *testing.T) p2phost.Host {
-	netw := netutil.GenSwarmNetwork(t, ctx)
+	netw := swarmt.GenSwarm(t, ctx)
 	return bhost.NewBlankHost(netw)
 }
 
