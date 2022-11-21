c7273ac2c78c1d783256bbc349928f255bb9cbcc
Marten Seemann <martenseemann@gmail.com>
2022-11-21 19:24:47 +1300

    update go-libp2p to v0.22.0

--- pubsub.go
+++ pubsub.go
@@ -9,9 +9,9 @@ import (
 	"sync"
 	"time"
 
-	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/libp2p/go-libp2p-core/routing"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/routing"
 
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	record "github.com/libp2p/go-libp2p-record"
